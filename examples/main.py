28712b6e3c298cf6635d32b5c208ad8c90ce36ec
Megaklis Vasilakis <megaklis.vasilakis@gmail.com>
2022-06-20 22:47:38 +0300

    Add full example

--- examples/main.py
+++ examples/main.py
@@ -1,12 +1,32 @@
 from fastapi import Depends, FastAPI
 from fastlogger.logger import FastLoggerMiddleware, FastLogger, Mode
 
+# Run with: uvicorn examples.main:dev --reload
+# Used to demonstrate the usage of the middleware in dev mode.
 
-app = FastAPI()
-app.add_middleware(FastLoggerMiddleware, mode=Mode.dev)
+dev = FastAPI()
+dev.add_middleware(FastLoggerMiddleware, mode=Mode.dev)
 
 
-@app.get("/")
+@dev.get("/")
+async def root(logger=Depends(FastLogger)):
+
+    logger.info("Hello World info")
+    logger.info({"message": "json as main log"})
+    logger.debug("Debuuuug", foo="foo", bar="bar")
+    logger.error("Erroooor!!!", obj={"foo": "bar"}, test="test")
+
+    return {"message": "Hello World"}
+
+
+# Run with: uvicorn examples.main:full --reload
+# Used to demonstrate the usage of the middleware in full mode.
+
+full = FastAPI()
+full.add_middleware(FastLoggerMiddleware, mode=Mode.full)
+
+
+@full.get("/")
 async def root(logger=Depends(FastLogger)):
 
     logger.info("Hello World info")
