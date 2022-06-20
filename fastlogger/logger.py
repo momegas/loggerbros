d869664898d29a4c3032ff71793bdf2a2e95015e
Megaklis Vasilakis <megaklis.vasilakis@gmail.com>
2022-06-20 21:33:24 +0300

    Clean up
    - stick with dev and prod config
    - add some docstrings

--- fastlogger/logger.py
+++ fastlogger/logger.py
@@ -8,7 +8,6 @@ import enum
 
 # creating enumerations using class
 class Mode(enum.Enum):
-    tiny = "tiny"
     dev = "dev"
     prod = "prod"
 
@@ -25,22 +24,30 @@ class FastLoggerMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
         self.mode = mode
 
+        # Get config according to mode
+        print_fn = self.get_print_fn()
+        min_log_level = self.get_min_log_level()
+
         structlog.configure(
             cache_logger_on_first_use=True,
-            wrapper_class=structlog.make_filtering_bound_logger(logging.DEBUG),
+            wrapper_class=structlog.make_filtering_bound_logger(min_log_level),
             logger_factory=structlog.BytesLoggerFactory(),
             processors=[
                 structlog.threadlocal.merge_threadlocal,
                 structlog.processors.add_log_level,
                 structlog.processors.format_exc_info,
                 structlog.processors.TimeStamper(fmt="iso", utc=True),
-                # TODO: change depending on mode
-                pretty_print_for_dev,
+                print_fn,
             ],
         )
         logger.info("Fastapi logger initialized.")
 
     async def dispatch(self, request: Request, call_next):
+        """
+        Used by FastAPI as middleware.
+        It creates a request id and adds it to the log as well as the url and method.
+        """
+
         request_id = str(uuid.uuid4())
         structlog.threadlocal.clear_threadlocal()
         structlog.threadlocal.bind_threadlocal(
@@ -57,3 +64,23 @@ class FastLoggerMiddleware(BaseHTTPMiddleware):
         logger.info(f"{request.url.__str__()} sent to loki")
 
         return response
+
+    def get_print_fn(self):
+        """Return the print function. Default is printing for dev mode."""
+
+        if self.mode == Mode.dev:
+            return pretty_print_for_dev
+        # elif self.mode == Mode.prod:
+        #     return pretty_print_for_prod
+        else:
+            return pretty_print_for_dev
+
+    def get_min_log_level(self):
+        """Return the minimum log level. Default is logging.DEBUG."""
+
+        if self.mode == Mode.dev:
+            return logging.DEBUG
+        elif self.mode == Mode.prod:
+            return logging.INFO
+        else:
+            return logging.INFO
